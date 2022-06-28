e9a51e07902960fd808df80bbcbd73b1aae3bd22
Abdollah Bakr <abdollah.bakr@gmail.com>
2022-06-28 10:26:19 +0200

    SMCT1-131 completed addresses view with data placeholders

---
+++ M-Commerce Shop/Set2/Me/Views/Cells/AddressCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  AddressCollectionViewCell.swift
+//  M-Commerce Shop
+//
+//  Created by Abdollah Bakr on 28/06/2022.
+//
+
+import UIKit
+
+class AddressCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "AddressCollectionViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+}
--- M-Commerce Shop/Set2/Me/Views/Controllers/AddressesViewController.swift
+++ M-Commerce Shop/Set2/Me/Views/Controllers/AddressesViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class AddressesViewController: UIViewController {
 
+    @IBOutlet weak var addressesCollectionView: UICollectionView!
     @IBOutlet weak var backButton: CircleButtonShadowView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        addressesCollectionView.delegate = self
+        addressesCollectionView.dataSource = self
+        
+        // Register CartItemCollectionViewCell from XIB
+        let addressNipCell = UINib(nibName: "AddressCollectionViewCell", bundle: nil)
+        addressesCollectionView.register(addressNipCell, forCellWithReuseIdentifier: AddressCollectionViewCell.identifier)
+        
         backButton.setTitle("", for: .normal)
     }
     
@@ -32,4 +40,27 @@ class AddressesViewController: UIViewController {
     @IBAction func backToSettings(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
+}
+
+
+extension AddressesViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier:  AddressCollectionViewCell.identifier, for: indexPath) as? AddressCollectionViewCell else { return AddressCollectionViewCell()}
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = collectionView.frame.size.width
+        let height = width/3
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    
 }
