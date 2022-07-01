8ed7ec5c42789531fbe8accb9a318065d2eb578b
Abdollah Bakr <abdollah.bakr@gmail.com>
2022-07-01 14:38:45 +0200

    SMCT1-58 added cart item count UI logic

--- M-Commerce Shop/Set2/Cart/Views/Cells/CartCollectionViewCell.swift
+++ M-Commerce Shop/Set2/Cart/Views/Cells/CartCollectionViewCell.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class CartCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var cartItemImageView: UIImageView!
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var itemPriceLabel: UILabel!
+    @IBOutlet weak var itemCountLabel: UILabel!
+    
     static let identifier = "CartCollectionViewCell"
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    @IBAction func stepItemCounter(_ sender: UIStepper) {
+        itemCountLabel.text = Int(sender.value).description
+    }
+    
 }
