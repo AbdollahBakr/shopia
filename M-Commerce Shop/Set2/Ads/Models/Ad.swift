9aa61c432da4540c4c8adbc2327a2cc096748ad5
Abdollah Bakr <abdollah.bakr@gmail.com>
2022-07-02 15:22:55 +0200

    SMCT1-16 added the ads initial model

---
+++ M-Commerce Shop/Set2/Ads/Models/Ad.swift
@@ -0,0 +1,17 @@
+//
+//  Add.swift
+//  M-Commerce Shop
+//
+//  Created by Abdollah Bakr on 02/07/2022.
+//
+
+import Foundation
+
+struct Ad {
+    let headline: String?
+    let advertiser: String?
+    let body: String?
+    let imageStrUrl: String?
+    let price: Float?
+    let strUrl: String?
+}
