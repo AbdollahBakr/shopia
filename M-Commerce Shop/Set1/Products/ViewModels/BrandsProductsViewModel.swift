c7e8070894d1bcf8bc6823afd7ce0930382a072a
Abdallah Eslah <abdallaheslah97@gmail.com>
2022-07-03 20:10:05 +0200

    added archeticture for products screen

---
+++ M-Commerce Shop/Set1/Products/ViewModels/BrandsProductsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  BrandsProductsViewModel.swift
+//  M-Commerce Shop
+//
+//  Created by Abdallah Eslah Mac on 03/07/2022.
+//
+
+import Foundation
+
+class BrandsProductsViewModel {
+    
+}
