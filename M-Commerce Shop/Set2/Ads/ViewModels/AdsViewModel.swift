65d2fbe715ab6f90a1f1f170e77e8bcb0560096c
Abdollah Bakr <abdollah.bakr@gmail.com>
2022-07-07 13:50:02 +0200

    SMCT1 added some ads with the ability to navigate to the associated ad webpage

---
+++ M-Commerce Shop/Set2/Ads/ViewModels/AdsViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  AdsViewModel.swift
+//  M-Commerce Shop
+//
+//  Created by Abdollah Bakr on 07/07/2022.
+//
+
+import Foundation
+
+class AdsViewModel {
+    static var ads = [Ad(headline: "Eid Ready. Get Up To 50% Off.", advertiser: "Lacoste", body: "body", imageStrUrl: "https://upload.wikimedia.org/wikipedia/commons/8/84/Logo_da_Lacoste.png", price: 0, strUrl: "https://www.lacoste.com.eg/en/homePage?"),
+                      Ad(headline: "Skechers Plus Anniversary! 15% OFF", advertiser: "Skechers", body: "body", imageStrUrl: "https://hyperpix.net/wp-content/uploads/2020/05/skechers-logo-font-free-download.jpg", price: 500, strUrl: "https://www.skechers.com/sale/"),
+                      Ad(headline: "SEMI-ANNUAL IS HERE | UP TO 50% OFF", advertiser: "Puma", body: "body", imageStrUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/4/49/Puma_AG.svg/2880px-Puma_AG.svg.png", price: 500, strUrl: "https://us.puma.com/us/en/puma/shop-all-markdowns"),
+                      Ad(headline: "GIVE 20%, GET 20%", advertiser: "Reebok", body: "body", imageStrUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Reebok_2019_logo.svg/2880px-Reebok_2019_logo.svg.png", price: 500, strUrl: "https://www.reebok.com/us"),
+    ]
+}
